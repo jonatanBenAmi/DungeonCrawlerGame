8b0a6ce87a1b19d83cd6bfc6fb7f3153c1b94202
Jonatan Ben-Ami <jbenam12@student.aau.dk>
2014-04-05 14:19:34 +0200

    updated controls
    changed the controls, to a more simpler version. Gravity is back on and
    the avatar is able to jump

--- New Unity Game/Assets/scripts/Controls.cs
+++ New Unity Game/Assets/scripts/Controls.cs
@@ -4,39 +4,56 @@ using System.Collections;
 [RequireComponent(typeof(CharacterController))]
 
 public class Controls : MonoBehaviour {
-	public float moveSpeed = 10; //used determine the move speed 10 instances per frame.
-	public Vector3 forwardDirection; // movedirection  return 3d coordinates
-	public Vector3 sideDirection2;
-	public CharacterController cc; //to declare our character
+	public float moveMentSpeed = 10.0f; //used determine the move speed 10 instances per frame.
+	public float verticalVelocity = 0;
+	public float jumpspeed = 10.0f;
 	
 	// Use this for initialization
 	void Start () {
-	cc = GetComponent<CharacterController>();
-		forwardDirection = Vector3.zero;
-		sideDirection2 = Vector3.zero;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
+		
+		//movement
+		 CharacterController cc = GetComponent<CharacterController>(); //to declare our character
+		
+		float forwardSpeed=Input.GetAxis ("Vertical")*moveMentSpeed; //taking input from the keyboard(up and down) and multiplying with speed
+		float sideSpeed= Input.GetAxis("Horizontal")*moveMentSpeed;
+		
+		verticalVelocity += Physics.gravity.y * Time.deltaTime;
+		
+	    if(cc.isGrounded && Input.GetButtonDown("Jump")){
+			verticalVelocity = jumpspeed;
+		}
+		
+		Vector3 speed = new Vector3(-sideSpeed, verticalVelocity,forwardSpeed);
+		
+		speed = transform.rotation * speed;
+		
+		
+
+       
+		
 	
-		forwardDirection = Vector3.forward * Input.GetAxis ("Vertical"); //vertical is a string that defines the buttons, to move from "up and and"
-		forwardDirection = transform.TransformDirection(forwardDirection).normalized; //transform: keyword to tell we are using this gameobject, 
-	                                                                       //normalized returns a value of 1 or -1, depending of up, down, left or right
-		forwardDirection *= moveSpeed;
-		
-		sideDirection2 = Vector3.left * Input.GetAxis ("Horizontal");
-		sideDirection2 = transform.TransformDirection(sideDirection2).normalized;
-		sideDirection2 *=moveSpeed;
-		
-	
 		
 		
-		cc.Move (forwardDirection * Time.deltaTime);   // uses the CharacterController script, moving it in the moveDirection
+		
+		cc.Move (speed * Time.deltaTime);   // uses the CharacterController script, moving it in the moveDirection
 		 
 	                                               //Time.deltaTime gives a smooth transission 
 		
-		cc.Move (sideDirection2* Time.deltaTime);
+		
+		
+
+		
+	
+		
+	
 	
 	
 	}
 }
+	
