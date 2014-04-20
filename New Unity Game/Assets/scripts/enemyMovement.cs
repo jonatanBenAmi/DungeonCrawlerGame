ec12b3a46f27c64b79952ba4118a5a8f795e2839
MalteHansen <Hansen257@hotmail.com>
2014-04-20 22:26:51 +0200

    Enemy Movement
    Adding movement to the enemy, making it look at the player and approach
    the player if he comes to close to the enemy.

---
+++ New Unity Game/Assets/scripts/enemyMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyMovement : MonoBehaviour {
+	public Transform Target;
+	public Transform myTransform;
+	public int MoveSpeed = 4;
+	public int MinDist = 2;
+	public float Distance;
+	// Use this for initialization
+	void Start () {
+		myTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Distance = Vector3.Distance (myTransform.position,Target.position);
+		if(Distance<30){
+			transform.LookAt (Target);
+			if(Vector3.Distance (transform.position,Target.position) >=MinDist){
+				transform.position += transform.forward*MoveSpeed*Time.deltaTime;
+			}
+		}
+
+	}
+}
