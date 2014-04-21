f0937e0da0eb2bba9eae2fb277b0b35453b22de0
MalteHansen <Hansen257@hotmail.com>
2014-04-21 18:28:57 +0200

    Enemy movement
    Making the enemy move randomely before seeing the player!

--- New Unity Game/Assets/scripts/enemyMovement.cs
+++ New Unity Game/Assets/scripts/enemyMovement.cs
@@ -14,30 +14,46 @@ public class enemyMovement : MonoBehaviour {
 	private Transform myTransform;
 	private float Distance;
 
+	public float ticTime;
+	public float RandNum;
+
+	public AudioClip MonsterSound;
+
 	// Use this for initialization
 	void Start () 
 	{
 		Target = GameObject.Find("Avatar");
 		timeToTurn = 0.0f;
 		MinDist = 0;
+		ticTime = 500.0f;
 		myTransform = transform;
 	}
 	
 	// Update is called once per frame
-	void Update () 
-	{
+	void Update () {
+		if(ticTime>0){
+			ticTime -= 1;
+		}else if(ticTime==0){
+			ticTime = 500;
+		}
+
 		Distance = Vector3.Distance (myTransform.position,Target.transform.position);
 
 		if(Distance<30)
 		{
+			audio.volume = 0.3f;
 			transform.LookAt (Target.transform);
+			transform.position += transform.forward*MoveSpeed*Time.deltaTime;
 
-			if(Vector3.Distance (transform.position,Target.transform.position) >=MinDist)
-			{
-				transform.position += transform.forward*MoveSpeed*Time.deltaTime;
+		}else{
+			audio.volume = 0f;
+			transform.position += transform.forward*1*Time.deltaTime;
+			if(ticTime==500){
+				RandNum = Random.Range (0,360);
+				Vector3 randomDirection = new Vector3 (0, RandNum, 0);
+				transform.Rotate (randomDirection);
 			}
 		}
-
 	}
 
 	
