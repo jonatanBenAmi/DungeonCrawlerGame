d476e823e661903ca5ce42bc9c20f39969c164d9
Jonatan Ben-Ami <jbenam12@student.aau.dk>
2014-04-29 14:45:58 +0200

    further comments
    now comments of the Dart_bullet script and, DestoryByBoundary and
    ammoBooster script

--- New Unity Game/Assets/scripts/Dart_Bullet.cs
+++ New Unity Game/Assets/scripts/Dart_Bullet.cs
@@ -5,20 +5,20 @@ public class Dart_Bullet : Bullet_Class {
 
 	public override void Start () 
 	{
-		damage = 3.0f;
-		penetrationPower = 3;
-		bulletVelocity = 40.0f;
+		damage = 3.0f;  //overall bullet damage to health
+		penetrationPower = 3; //how many enemies the bullet can penetrate
+		bulletVelocity = 40.0f; //Bullet Velocity
 		
-		rigidbody.velocity = transform.forward * bulletVelocity;
+		rigidbody.velocity = transform.forward * bulletVelocity; //make the bullet shoot forward
 	}
 	
 	
 	public override void Update () 
 	{
-		if(transform.position.y > 10.0f){
-			Destroy(gameObject);
-		}else if(penetrationPower < 1){
-			Destroy(gameObject);
+		if(transform.position.y > 10.0f){ //if the bullet is located higher than 10.0f 
+			Destroy(gameObject); //then destory the bullet
+		}else if(penetrationPower < 1){ //if the penetration power is less than 1
+			Destroy(gameObject); //destroy the bullet
 		}
 	}
 	
@@ -28,19 +28,19 @@ public class Dart_Bullet : Bullet_Class {
 		
 		if(other.tag == "Enemy")
 		{
-			penetrationPower--;
-			collisionObject = other.gameObject;
+			penetrationPower--; //if the bullet hits an enemy, it will only be able to penetrate two more 
+			collisionObject = other.gameObject; //get the characteristics of the enemy gameobject
 			Charactor_Class script = collisionObject.GetComponent<Charactor_Class>();
-			bool isArmored = (script.armorStrength > 0)? true:false;
+			bool isArmored = (script.armorStrength > 0)? true:false; //evaluates
 
-			if(isArmored)
+			if(isArmored)  //if this is set to true
 			{
-				script.armorStrength -= 3 * damage;
+				script.armorStrength -= 3 * damage; //then this will happen
 				script.defence -= damage;
 			}
 			else
 			{
-				script.defence -= damage;
+				script.defence -= damage; 
 			}
 		}
 		if(other.tag == "Player")
