4cccf3c5957cd7d7f46cbff70fca60d64f91af5d
Jonatan Ben-Ami <jbenam12@student.aau.dk>
2014-04-29 14:05:00 +0200

    films and comments
    a few videos of the gameplay has been recorded, uploaded on youtube and
    is added to the wiki on github. the AK47_weapon script has been updated
    with comments

--- New Unity Game/Assets/scripts/Ak47_Weapon.cs
+++ New Unity Game/Assets/scripts/Ak47_Weapon.cs
@@ -3,61 +3,61 @@ using System.Collections;
 
 public class Ak47_Weapon : Weapons_Class 
 {
-	public Event_Timer boosterTimer;
+	public Event_Timer boosterTimer; //timer used to limit the amount of time for rateOfFire booster
 
 	public override void Start()
 	{
-		weaponTimer = new Event_Timer(rateOfFire,true);
-		boosterTimer = new Event_Timer(rateOfFire,false);
-		ammunitionChoise = 0;
-		ammunitionStock = new int[4]{100,100,100,100};
+		weaponTimer = new Event_Timer(rateOfFire,true); //constructor to the timer of the fireRate
+		boosterTimer = new Event_Timer(rateOfFire,false);//constuctor to time of the booster timer
+		ammunitionChoise = 0; //variable that stores what number of bullet type your are in possession of
+		ammunitionStock = new int[4]{100,100,100,100}; //array that holds the amounth of bullets for each type of bullet
 	}
-	public override void FixedUpdate()
+	public override void FixedUpdate() //override function to the weapon Class
 	{
 
-		Debug.Log(weaponTimer.eventTimer());
-		if (weaponTimer.eventTimer())
+		Debug.Log(weaponTimer.eventTimer()); //This is a useful tool when we had problems
+		if (weaponTimer.eventTimer())//if this is true, that we can shoot bullets
 		{
 			triggerTick = true;
 
 		}
-		if (boosterTimer.eventTimer())
+		if (boosterTimer.eventTimer()) //resets after booster
 		{
-			rateOfFire = 15f;
-			boosterTimer.TimerIsOn = false;
+			rateOfFire = 15f; //reset firerate to 15
+			boosterTimer.TimerIsOn = false; //turns of the timer
 		}
-		if (Input.GetButton ("Fire1")) 
+		if (Input.GetButton ("Fire1")) //if the fire button on the mouse is pressed
 		{
-			if (triggerTick)
+			if (triggerTick) //check if this is true (if the timer is true and your gun is loaded)
 			{
-				fireWeapon();
-				triggerTick = false;
-				weaponTimer.TimeToNextTick = rateOfFire;
+				fireWeapon();  //call the fire weapon function   
+				triggerTick = false; //resets the weapons timer
+				weaponTimer.TimeToNextTick = rateOfFire; // the next two resets the timer
 				weaponTimer.TimeTicking = 0f;
 			}
 		}
-		if(Input.GetAxis("Mouse ScrollWheel") != 0)
+		if(Input.GetAxis("Mouse ScrollWheel") != 0) //the mouse wheel is scrolled and the scoll value is not 0
 		{
-			ammunitionChoise += (int)Input.GetAxisRaw("Mouse ScrollWheel");
-			if(ammunitionChoise > ammunitionStock.Length - 1){
-				ammunitionChoise = 0;
-			}else if (ammunitionChoise < 0){
+			ammunitionChoise += (int)Input.GetAxisRaw("Mouse ScrollWheel"); //ammunition choise corresponces to the mouse scrollwheel sensivity value
+			if(ammunitionChoise > ammunitionStock.Length - 1){ //if the choice is bigger than the options available,
+				ammunitionChoise = 0; //then reset it to the first choise
+			}else if (ammunitionChoise < 0){ //if it is less than 0(the first choise), then resets to the last possible weapon option
 				ammunitionChoise = ammunitionStock.Length - 1;
 			}
 		}
 	}
-	public override void fireWeapon()
+	public override void fireWeapon() //function that chooses the a type of ammunition and fire the right amounth of bullets
 	{
-		if(ammunitionStock[ammunitionChoise] > 0)
+		if(ammunitionStock[ammunitionChoise] > 0) //choose the ammunition type
 		{
-			audio.PlayOneShot (gunFire);
-			ammunitionStock[ ammunitionChoise]--;
-			Instantiate(ammunitionType[ammunitionChoise], barrelEnd.position, barrelEnd.rotation);
+			audio.PlayOneShot (gunFire); //plays the gunfire sound when a shot is fired
+			ammunitionStock[ ammunitionChoise]--; //when a shot is fired, you have one bullet less
+			Instantiate(ammunitionType[ammunitionChoise], barrelEnd.position, barrelEnd.rotation); //spawns the bullet from the tip of the barrel position
 		}else if(ammunitionStock[ammunitionChoise]==0){
 			audio.PlayOneShot (gunEmpty);
 		}
 	}
-	void OnGUI()
+	void OnGUI() // whole GUI function displaying the type of bullet accessed and how many bullets available
 	{ 
 		GUI.TextField(new Rect(Screen.width - 160,0,160,80),	ammunitionType[ammunitionChoise].name + ": \n" +
 		              ammunitionStock[ ammunitionChoise] + "x ");
