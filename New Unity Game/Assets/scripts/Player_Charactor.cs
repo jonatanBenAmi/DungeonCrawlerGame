37af5c10e1c23e5d198253be6d003e9691bacd61
UlrikBronnum <ull84@live.dk>
2014-09-28 19:01:36 +0200

    save/load added
    added initial save/load functions

--- New Unity Game/Assets/scripts/Player_Charactor.cs
+++ New Unity Game/Assets/scripts/Player_Charactor.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
+using System.Globalization;
+
 
 // imports a characterController
 [RequireComponent(typeof(CharacterController))] 
@@ -72,6 +75,7 @@ public class Player_Charactor : Charactor_Class
 		checkPointCount = 0;
 
 		cc = GetComponent<CharacterController>();
+		gameLoad();
 	}
 
 	public override void Update () 
@@ -80,6 +84,7 @@ public class Player_Charactor : Charactor_Class
 		// to quit the game
 		if(Input.GetKey(KeyCode.Escape))
 		{
+			gameSave();
 			Application.Quit();
 		}
 		// to pause the game
@@ -95,6 +100,8 @@ public class Player_Charactor : Charactor_Class
 		if(paused)
 		{
 			Time.timeScale = 0;
+
+			//Debug.Log(Application.dataPath);
 		}else
 		{
 			Time.timeScale = 1;
@@ -290,4 +297,80 @@ public class Player_Charactor : Charactor_Class
 		get {return enemyKills;}
 		set {enemyKills = value;}
 	}
+
+
+	public void gameSave(){
+		// Find path of game folder and save it
+		string path = Application.dataPath + "/SaveGame";
+		// Check if the folder does not exists, if not 
+		// then create it at the path!
+		if(!Directory.Exists(path)){
+			DirectoryInfo dir = Directory.CreateDirectory(path);
+		}
+		// Check if the file does not exists, if not 
+		// then create it!
+		if(!File.Exists(path + "/profileSave.fzf")){
+			StreamWriter file = File.CreateText(path + "/profileSave.fzf");
+			file.Close();
+		}
+		// Load the savegame file and write the data to it
+		StreamWriter loadFile = new StreamWriter(path + "/profileSave.fzf");
+		// Data comes from the custom function saveFileFormat
+		loadFile.WriteLine(saveFileFormat());
+		// Close the file
+		loadFile.Close();
+
+	}
+	public void gameLoad(){
+		// Find path of folder 
+		string path = Application.dataPath + "/SaveGame/";
+		// If the file exists 
+		if(File.Exists(path + "/profileSave.fzf")){
+			// Load file
+			StreamReader fileLoaded = File.OpenText(path + "/profileSave.fzf");
+			string s = "";
+			// while there is a new line read it
+			while((s = fileLoaded.ReadLine()) != null){
+				// Split the line at the '='
+				string[] getLine = s.ToString().Split('=');
+				// Match title to variable and update game
+				if(getLine[0] == "Player Position X"){ 
+					Vector3 temp = transform.position;
+					temp.x = float.Parse(getLine[1].ToString(), System.Globalization.CultureInfo.InvariantCulture);
+					transform.position = temp;
+				}
+				if(getLine[0] == "Player Position Y"){
+					Vector3 temp = transform.position;
+					temp.y = float.Parse(getLine[1].ToString(), System.Globalization.CultureInfo.InvariantCulture);
+					transform.position = temp;
+				}
+				if(getLine[0] == "Player Position Z"){
+					Vector3 temp = transform.position;
+					temp.z = float.Parse(getLine[1].ToString(), System.Globalization.CultureInfo.InvariantCulture);
+					transform.position = temp;
+				}
+				if(getLine[0] == "Player Lives"){
+					lives = int.Parse(getLine[1]);
+				}
+				if(getLine[0] == "Player Defence"){
+					defence = float.Parse(getLine[1].ToString(), System.Globalization.CultureInfo.InvariantCulture);
+				}
+			}
+			// Close the file
+			fileLoaded.Close();
+		}
+	}
+	public string saveFileFormat(){
+		// The file is a long string 
+		string data = "";
+		// adds lines to it
+		data += "Player Position X=" + transform.position.x + "\n";
+		data += "Player Position Y=" + transform.position.y + "\n";
+		data += "Player Position Z=" + transform.position.z + "\n";
+		data += "Player Lives=" + lives + "\n";
+		data += "Player Defence=" + defence + "\n";
+		// returns the string when done
+		return data;
+	}
 }
+
